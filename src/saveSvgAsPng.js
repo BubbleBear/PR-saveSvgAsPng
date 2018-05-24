2ca7a00d7af807b79a1be73b4922b389fb864ed2
Eric Shull <eric@exupero.org>
2018-05-24 17:39:03 -0400

    Restore compatibility with ES5.

--- src/saveSvgAsPng.js
+++ src/saveSvgAsPng.js
@@ -1,3 +1,5 @@
+import 'babel-polyfill';
+
 (function() {
   const out$ = typeof exports != 'undefined' && exports || typeof define != 'undefined' && {} || this;
   if (typeof define !== 'undefined') define(() => out$);
